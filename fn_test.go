5496cd544533653f97eac0e0ee5b8a1979baee9a
Nic Cope <nicc@rk0n.org>
2024-09-19 15:36:49 -0700

    Bump function-sdk-go to v0.3.0
    This requires updating to v1 RPC types.
    
    The update also brings in the latest crossplane-runtime, and k8s.io
    v0.30.0 dependencies. v0.31.0 is out, but we can't use them until
    crossplane-runtime is updated.
    
    Signed-off-by: Nic Cope <nicc@rk0n.org>

--- fn_test.go
+++ fn_test.go
@@ -11,7 +11,7 @@ import (
 
 	"github.com/crossplane/crossplane-runtime/pkg/logging"
 
-	fnv1beta1 "github.com/crossplane/function-sdk-go/proto/v1beta1"
+	fnv1 "github.com/crossplane/function-sdk-go/proto/v1"
 	"github.com/crossplane/function-sdk-go/resource"
 	"github.com/crossplane/function-sdk-go/response"
 )
@@ -20,10 +20,10 @@ func TestRunFunction(t *testing.T) {
 
 	type args struct {
 		ctx context.Context
-		req *fnv1beta1.RunFunctionRequest
+		req *fnv1.RunFunctionRequest
 	}
 	type want struct {
-		rsp *fnv1beta1.RunFunctionResponse
+		rsp *fnv1.RunFunctionResponse
 		err error
 	}
 
@@ -35,10 +35,10 @@ func TestRunFunction(t *testing.T) {
 		"AutoDetectReadiness": {
 			reason: "An existing composed resource with unspecified readiness and a Ready: True status condition should be detected as ready",
 			args: args{
-				req: &fnv1beta1.RunFunctionRequest{
-					Meta: &fnv1beta1.RequestMeta{Tag: "hello"},
-					Observed: &fnv1beta1.State{
-						Composite: &fnv1beta1.Resource{
+				req: &fnv1.RunFunctionRequest{
+					Meta: &fnv1.RequestMeta{Tag: "hello"},
+					Observed: &fnv1.State{
+						Composite: &fnv1.Resource{
 							Resource: resource.MustStructJSON(`{
 								"apiVersion": "test.crossplane.io/v1",
 								"kind": "TestXR",
@@ -47,7 +47,7 @@ func TestRunFunction(t *testing.T) {
 								}
 							}`),
 						},
-						Resources: map[string]*fnv1beta1.Resource{
+						Resources: map[string]*fnv1.Resource{
 							"ready-composed-resource": {
 								Resource: resource.MustStructJSON(`{
 									"apiVersion": "test.crossplane.io/v1",
@@ -68,8 +68,8 @@ func TestRunFunction(t *testing.T) {
 							},
 						},
 					},
-					Desired: &fnv1beta1.State{
-						Resources: map[string]*fnv1beta1.Resource{
+					Desired: &fnv1.State{
+						Resources: map[string]*fnv1.Resource{
 							// This function doesn't care about the desired
 							// resource schema. In practice it would match
 							// observed (without status), but for this test it
@@ -82,17 +82,17 @@ func TestRunFunction(t *testing.T) {
 				},
 			},
 			want: want{
-				rsp: &fnv1beta1.RunFunctionResponse{
-					Meta: &fnv1beta1.ResponseMeta{Tag: "hello", Ttl: durationpb.New(response.DefaultTTL)},
-					Desired: &fnv1beta1.State{
-						Resources: map[string]*fnv1beta1.Resource{
+				rsp: &fnv1.RunFunctionResponse{
+					Meta: &fnv1.ResponseMeta{Tag: "hello", Ttl: durationpb.New(response.DefaultTTL)},
+					Desired: &fnv1.State{
+						Resources: map[string]*fnv1.Resource{
 							// This function doesn't care about the desired
 							// resource schema. In practice it would match
 							// observed (without status), but for this test it
 							// doesn't matter.
 							"ready-composed-resource": {
 								Resource: resource.MustStructJSON(`{}`),
-								Ready:    fnv1beta1.Ready_READY_TRUE,
+								Ready:    fnv1.Ready_READY_TRUE,
 							},
 						},
 					},
